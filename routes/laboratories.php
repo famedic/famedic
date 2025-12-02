49275f95aa3cffbd22b5ede5152bb80c27e91243
AdminFamedic <admin@famedic.com.mx>
2025-12-02 11:07:34 -0600

    routes laboratories

--- routes/laboratories.php
+++ routes/laboratories.php
@@ -78,11 +78,20 @@ Route::middleware([
     */
 
 
-    Route::get('/laboratory-results', [LaboratoryResultController::class, 'index'])->name('laboratory-results.index');
-    Route::post('/laboratory-results/{type}/{id}/mark-downloaded', [LaboratoryResultController::class, 'markAsDownloaded'])->name('laboratory-results.mark-downloaded');
-    Route::get('/laboratory-results/{type}/{id}/download', [LaboratoryResultController::class, 'download'])->name('laboratory-results.download');
-    Route::get('/laboratory-results/{type}/{id}/view', [LaboratoryResultController::class, 'view'])->name('laboratory-results.view');
+    //Route::get('/laboratory-results', [LaboratoryResultController::class, 'index'])->name('laboratory-results.index');
+    
+    //Route::get('/laboratory-results/{type}/{id}/download', [LaboratoryResultController::class, 'download'])->name('laboratory-results.download');
+    //Route::get('/laboratory-results/{type}/{id}/view', [LaboratoryResultController::class, 'view'])->name('laboratory-results.view');
 
     
     Route::post('/laboratory-results/notification/{notification}/mark-read', [LaboratoryResultController::class, 'markAsRead']);
+
+    Route::prefix('laboratory-results')->group(function () {
+    Route::get('/', [LaboratoryResultController::class, 'index'])->name('laboratory-results.index');
+    Route::post('/notification/{notification}/mark-read', [LaboratoryResultController::class, 'markAsRead'])->name('laboratory-results.mark-read');
+    Route::post('/notification/{notification}/refresh', [LaboratoryResultController::class, 'refreshResults'])->name('laboratory-results.refresh');
+    Route::get('/{type}/{id}/view', [LaboratoryResultController::class, 'view'])->name('laboratory-results.view');
+    Route::get('/{type}/{id}/download', [LaboratoryResultController::class, 'download'])->name('laboratory-results.download');
 });
+});
+
