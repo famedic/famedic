969e475f555aab6a218c771ce856ec6eaad1f4ac
AdminFamedic <admin@famedic.com.mx>
2026-03-22 15:21:13 -0600

    benefit comment layout auth

--- resources/js/Layouts/AuthLayout.jsx
+++ resources/js/Layouts/AuthLayout.jsx
@@ -68,12 +68,12 @@ export default function AuthLayout({
 													</svg>
 													<span className="text-sm sm:text-base">Estudios de laboratorio e imagen con descuento de hasta 60%</span>
 												</li>
-												<li className="flex items-start gap-3">
+												{/*<li className="flex items-start gap-3">
 													<svg className="mt-0.5 h-5 w-5 flex-shrink-0" fill="currentColor" viewBox="0 0 20 20">
 														<path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
 													</svg>
 													<span className="text-sm sm:text-base">Solicita tus medicamentos directo a tu(s) domicilio(s)</span>
-												</li>
+												</li>*/}
 												<li className="flex items-start gap-3">
 													<svg className="mt-0.5 h-5 w-5 flex-shrink-0" fill="currentColor" viewBox="0 0 20 20">
 														<path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
