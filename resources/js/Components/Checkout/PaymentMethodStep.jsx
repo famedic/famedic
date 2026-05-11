6c4a611ef2b679861afce1b5a826d786a6bd8776
AdminFamedic <admin@famedic.com.mx>
2026-05-11 17:54:19 -0600

    fix file front

--- resources/js/Components/Checkout/PaymentMethodStep.jsx
+++ resources/js/Components/Checkout/PaymentMethodStep.jsx
@@ -24,6 +24,7 @@ export default function PaymentMethodStep({
     const selectedPaymentMethod = useMemo(() => {
         if (data.payment_method === "coupon_balance") {
             return "coupon_balance";
+        }
         if (data.payment_method === "paypal") {
             return "paypal";
         }
